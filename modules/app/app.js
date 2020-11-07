94018426e4b86016aa052290b581247a9addb8ac
Trevor Alt <trevorjalt@gmail.com>
2020-11-06 22:01:45 -0500

    testing

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const helmet = require('helmet')
 const peopleRouter = require('../people/people-router')
 const petsRouter = require('../pets/pets-router')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 app.use(helmet())
 
 app.use('/api/people', peopleRouter)
